8f8426c9fba70486b0475d60f8807baac36ba72a
David Christie <davidchristie@users.noreply.github.com>
2019-12-01 19:57:45 +0100

    Refactor entity component tests

--- src/factories/createEntityComponents.test.tsx
+++ src/factories/createEntityComponents.test.tsx
@@ -3,16 +3,20 @@ import React from "react";
 import model from "../testing/model";
 import { describeRender } from "../testing/render";
 
-describe("entity model components", () => {
-  describeRender("create page", <model.CreatePage />, getResult => {
-    it("renders the Create component", () => {
-      getResult().getByTestId("Create");
+const describeComponent = (
+  name: string,
+  element: React.ReactElement,
+  testId: string
+) => {
+  describeRender(name, element, getResult => {
+    it(`renders the ${testId} component`, () => {
+      getResult().getByTestId(testId);
     });
   });
+};
 
-  describeRender("list page", <model.ListPage />, getResult => {
-    it("renders the List component", () => {
-      getResult().getByTestId("List");
-    });
-  });
+describe("entity model components", () => {
+  describeComponent("create page", <model.CreatePage />, "Create");
+
+  describeComponent("list page", <model.ListPage />, "List");
 });
